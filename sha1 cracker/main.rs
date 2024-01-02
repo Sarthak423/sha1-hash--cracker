edf6125a65527f43a556c41a1bc5013c68a173fc
Sarthak423 <114341848+Sarthak423@users.noreply.github.com>
2024-01-02 10:57:40 +0000

    Add files via upload

---
+++ sha1 cracker/main.rs
@@ -0,0 +1,13 @@
+use std::{
+    env,
+    error::Error,
+    
+fn main() {
+    let args: Vec<string> = env: :args() .collect();
+    
+    if arge.len() !=3 {
+        println!("Usage:");
+        println!("sha1_cracker: <wordlist.txt> <sha1_hash>");
+        return;
+    }
+}
