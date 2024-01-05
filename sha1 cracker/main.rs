6f4346c4dc085c6d9afed135de70354bbc80c734
Cyber 404 <114341848+cyber4044@users.noreply.github.com>
2024-01-05 05:59:47 +0000

    main.rs

--- sha1 cracker/main.rs
+++ sha1 cracker/main.rs
@@ -1,13 +1,39 @@
+use sha1::Digest;
 use std::{
     env,
     error::Error,
-    
-fn main() {
+    fs::file,
+    io: :{Bufread,Bufreader},
+}
+    const SHA1_HEX_STRING_LENGTH: usize = 40;
+
+fn main()  result<(), Box<dyn Error>>{
     let args: Vec<string> = env: :args() .collect();
     
     if arge.len() !=3 {
         println!("Usage:");
         println!("sha1_cracker: <wordlist.txt> <sha1_hash>");
-        return;
+        return ok(());
+    }
+
+    let hash_to_crack=args{2}.trim();
+    if hash_to_crack.len() != SHA1_HEX_STRING_LENGTH  {
+        return err("sh1 hash is not vaild".into())
+    }
+    let wordlist_file = file::open(&args[1])?;
+    let reader = Bufreader::new(&wordlist_file)
+
+    for line in reader.lines(){
+        let line = line?;
+        let common_password = line.trim();
+        if hash_to_crack ===
+        &hex::encode(sha1::sha1::digest(common_password.as_bytes())){
+            println!("passwordfound: {}", &common_password);
+            return ok(());
+        }    
+    }
+        println!("password not found in wordlist :(");
+
+    ok(())
     }
 }
